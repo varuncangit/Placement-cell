8a62a7ddbe9a6915971b0267df9198a5943fe1ae
varuncangit <varunshuklaweb@gmail.com>
2024-05-14 17:08:10 +0530

    placement-cell

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -3,5 +3,5 @@ const Student = require('../models/studentSchema.js');
 // render home page
 module.exports.homePage = async function (req, res) {
   const students = await Student.find({});
-  return res.render('home', { students });
+  return res.render('index', { students });
 };
